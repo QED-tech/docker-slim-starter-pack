6c93b5791a3f40d1149eafb621444f572d2ee2db
Vladislav <qedirast@gmail.com>
2021-07-04 14:52:35 +0700

    Added container

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,26 @@
 
 declare(strict_types=1);
 
-$result = new stdClass();
-$result->name = 'qed';
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-echo json_encode($result);
+require __DIR__ . '/../vendor/autoload.php';
+
+http_response_code(500);
+
+$container = new Container();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), false, false);
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+	$response->getBody()->write(json_encode([
+		'name' => 'qed'
+	]));
+	return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
